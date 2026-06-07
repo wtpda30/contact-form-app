ac102819589549cc70966131496de3524eae8742
miyako <wtpda30@gmail.com>
2026-06-08 00:47:11 +0900

    feat: タグ管理一連の実装 #12

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,23 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
 use App\Models\Contact;
 use App\Models\Category;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/',[ContactController::class,'index'])->name('contact.index');
 Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
 Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('contact.thanks');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/admin/tags', [TagController::class,'store']);
+    Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit']);
+    Route::put('/admin/tags/{tag}', [TagController::class, 'update']);
+    Route::delete('/admin/tags/{tag}', [TagController::class, 'destroy']);
+});
 
 //仮ルート:コントローラー作ってから書き換え
 Route::middleware('auth')->group(function () {
