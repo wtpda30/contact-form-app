c24876525e3b3718eb8b3365ccad0ec0cf91d40a
miyako <wtpda30@gmail.com>
2026-07-07 01:37:54 +0900

    feat: 全体的な修正 #42

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +27,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($request->is('api/*') && $e instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'お問い合わせが見つかりませんでした。',
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
